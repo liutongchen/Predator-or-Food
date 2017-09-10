52f76b262c8b56b542c904dbd5bb6aa0d1b5f01c
Liutong Chen <liutong.clt@gmail.com>
2017-09-09 21:50:05 -0700

    move app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./server/public'));
+app.use(express.static('./public'));
 
 app.listen(port, () => {
     console.log(`Predator or Food is running on port ${port}.`);
